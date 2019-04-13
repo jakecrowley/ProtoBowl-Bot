8c6c44b223032e4f8ac9dea5bb6a1e712934eab5
Jake Crowley <jakecrow@gmail.com>
2019-04-13 13:38:27 -0400

    Added ping handler to prevent disconnecting

--- ProtoBowl Bot/SocketIOClient.cs
+++ ProtoBowl Bot/SocketIOClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Websocket.Client;
 
@@ -12,6 +13,18 @@ namespace ProtoBowl_Bot
         private int mc = 0;
 
         public SocketIOClient(Uri uri) : base(uri) { }
+
+        public new Task Start() 
+        {
+            MessageReceived.Subscribe((message) => {
+                if (message.Text.StartsWith("2::"))
+                {
+                    Send("2::");
+                }
+            });
+
+            return base.Start();
+        }
 
         public void SocketIOSend(string message)
         {
